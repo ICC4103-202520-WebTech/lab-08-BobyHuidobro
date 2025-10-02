dea09a933f09a4aba9b033ed3526be77c3bb28e9
BobyHuidobro <dhuidobro@miuandes.cl>
2025-10-02 00:20:11 -0300

    aplicación

---
+++ recipieBook/app/models/recipe.rb
@@ -0,0 +1,7 @@
+class Recipe < ApplicationRecord
+    validates :title, presence: true
+    validates :cook_time, presence: true
+    validates :difficulty, presence: true
+
+    has_rich_text :instructions
+end
